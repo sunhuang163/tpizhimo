4d606810d9dffb760e0123291d7bb8555d77e16b
banfg56 <banfg56@yeah.net>
2015-06-08 08:27:55 +0800

    Site template style change

--- app/Lib/Model/ContentViewModel.class.php
+++ app/Lib/Model/ContentViewModel.class.php
@@ -3,7 +3,7 @@ class TagsViewModel extends ViewModel {
   	protected $viewFields = array (
 		 'content'=>array('*','_type'=>"LEFT"),
 		 'nclass'=>array('*', '_on'=>'nclass.ncid = content.ncid','_type'=>"LEFT"),
-		 'nchapter' => array('*',"_on"=>"content.cpid=nchapter.cpid",'_type'=>"LEFT");
+		 'nchapter' => array('*',"_on"=>"content.cpid=nchapter.cpid",'_type'=>"LEFT"),
 		 'novel' => array('*','_on'=>'novel.nid = content.nid'),
  	);
 }
